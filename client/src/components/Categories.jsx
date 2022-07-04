d0f158c5734e58255abe96f05d4f5a704299d126
Akanksha <61158791+Akanksha-04@users.noreply.github.com>
2022-07-05 00:56:00 +0530

    Update Categories.jsx

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -4,7 +4,15 @@ import { categories } from '../data';
 import CategoryItem from './CategoryItem';
 import { mobile } from "../responsive";
 
-const Container = styled.div`
+const Container = styled.div``;
+  
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+  margin: 20px;
+`;
+
+const Wrapper = styled.div`
     display: flex;
     padding: 20px;
     justify-content: space-between;
@@ -13,9 +21,12 @@ const Container = styled.div`
 
 const Categories = () => {
   return <Container>
+       <Title>SHOP BY CATEGORY</Title>
+        <Wrapper>
         {categories.map(item =>(
             <CategoryItem item={item} key={item.id}/>
         ))}
+        </Wrapper>
     </Container>;
   
 }
